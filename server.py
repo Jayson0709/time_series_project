09a54cefd585eef1a9cf9756eafe1401b466ee63
Jayson Zhou <jaysonz6879@gmail.com>
2022-01-29 23:12:40 +0000

    [Add] Flask server file.

---
+++ server.py
@@ -0,0 +1,36 @@
+from flask.json import jsonify
+from flask import Flask, render_template
+from flask import request
+import pandas as pd
+from pyecharts import options as opts
+from pyecharts.charts import Line
+
+n = r"./dataset/Beef_TRAIN"
+data = pd.read_csv(n)
+print(data)
+
+app = Flask(__name__, static_folder=r"./templates")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/summary")
+def summary():
+    return render_template("summary.html")
+
+
+@app.route("/readme")
+def readme():
+    return render_template("README.html")
+
+
+@app.route("/visualization")
+def visualization():
+    return render_template("visualization.html")
+
+
+if __name__ == "__main__":
+    app.run()
