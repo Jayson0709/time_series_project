7db370c6bb128ca2cf0f8c20fa853bd8d12e317a
Jayson Zhou <jaysonz6879@gmail.com>
2022-02-01 22:40:10 +0000

    [Update] update the script for the PAA algorithm.

--- static/Python/paa.py
+++ static/Python/paa.py
@@ -1,12 +1,21 @@
 # Piecewise Aggregate Approximate
+from pyts.approximation import PiecewiseAggregateApproximation
+import pandas as pd
 
-import numpy as np
+# TODO In the webpage, use a slide for user input to define the value of "window_size"
+
+# Get sample data
+n = r"../../datasets/Beef_TRAIN"
+data = pd.read_csv(n).to_numpy()
 
 
-def paa(time_series, paa_size):
-    length = len(time_series)
-    if time_series == paa_size:
-        return time_series
-    if length % paa_size == 0:
-        return
-    return
+def paa(time_series_data, window_size):
+    # PAA transformation based on the input window_size
+    # if window_size is 1,
+    # if window_size is n, return the average of the time series data.
+    paa_set = PiecewiseAggregateApproximation(window_size=window_size)
+    X_paa = paa_set.transform(time_series_data)
+    return X_paa
+
+
+print(paa(data, 6))
