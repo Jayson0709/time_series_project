6cb2c817783d3c6387955fc4fd41d5bf2432b60c
Jayson Zhou <jaysonz6879@gmail.com>
2022-02-01 22:40:20 +0000

    [Update] update the script for the DWT algorithm.

--- static/Python/dwt.py
+++ static/Python/dwt.py
@@ -1,4 +1,18 @@
 # Discrete Wavelet Transformation
 
-import numpy as np
+import pywt
+import pandas as pd
+
+# Get sample data
+n = r"../../datasets/Beef_TRAIN"
+data = pd.read_csv(n).to_numpy()
+
+
+def dwt(time_series_data):
+    # Be default, we would use 'db1' as our wavelet object
+    # the DWT function will return approximation and detail coefficients
+    return pywt.dwt(time_series_data, 'db1')
+
+
+print(dwt(data))
 
