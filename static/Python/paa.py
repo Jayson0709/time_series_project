9d99296a9fa538a0b6782a9f1f2a089328b9844c
Jayson Zhou <jaysonz6879@gmail.com>
2022-02-03 22:50:19 +0000

    [Update] re-implement the PAA algorithm.

--- static/Python/paa.py
+++ static/Python/paa.py
@@ -2,20 +2,50 @@
 from pyts.approximation import PiecewiseAggregateApproximation
 import pandas as pd
 
-# TODO In the webpage, use a slide for user input to define the value of "window_size"
-
 # Get sample data
 n = r"../../datasets/Beef_TRAIN"
-data = pd.read_csv(n).to_numpy()
+data = pd.read_csv(n).to_numpy()[3]
 
 
-def paa(time_series_data, window_size):
-    # PAA transformation based on the input window_size
-    # if window_size is 1,
-    # if window_size is n, return the average of the time series data.
-    paa_set = PiecewiseAggregateApproximation(window_size=window_size)
-    X_paa = paa_set.transform(time_series_data)
-    return X_paa
+# TODO In the webpage, use a slide for user input to define the value of "segments"
+def paa_transformation(time_series_data, segments):
+    # PAA transformation based on the input segments
+    if segments < 1:
+        raise ValueError('Segments value is too small.')
+    length = len(time_series_data)
+    if length < segments:
+        raise ValueError('input array length is smaller than segments value.')
 
+    reduced_data = [None] * segments
+    segment_size = length / segments
+    if length % segments == 0:
+        local_sum = 0
+        _n = 0
+        for i in range(length):
+            local_sum += time_series_data[i]
+            if (i + 1) % segment_size == 0:
+                reduced_data[_n] = round(local_sum / segment_size, 3)
+                _n += 1
+                if _n == segments:
+                    break
+                local_sum = 0
+    else:
+        k = 0
+        local_sum = 0
+        for i in range(segments - 1):
+            _x = (i + 1) * segment_size - 1
+            while k < _x:
+                local_sum += time_series_data[k]
+                k += 1
+            delta = _x - int(_x)
+            local_sum += delta * time_series_data[k]
+            reduced_data[i] = round(local_sum / segment_size, 3)
 
-print(paa(data, 6))
+            local_sum = (1 - delta) * time_series_data[k]
+            k += 1
+
+        while k < length:
+            local_sum += time_series_data[k]
+            k += 1
+        reduced_data[segments - 1] = round(local_sum / segment_size, 3)
+    return reduced_data
