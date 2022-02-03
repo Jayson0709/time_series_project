adc7c5d610977e8c219a75c09296057fff6c5aca
Jayson Zhou <jaysonz6879@gmail.com>
2022-02-03 23:07:40 +0000

    [Update] change the wavelet object for dwt transformation.

--- static/Python/dwt.py
+++ static/Python/dwt.py
@@ -1,5 +1,4 @@
 # Discrete Wavelet Transformation
-
 import pywt
 import pandas as pd
 
@@ -8,11 +7,9 @@ n = r"../../datasets/Beef_TRAIN"
 data = pd.read_csv(n).to_numpy()
 
 
-def dwt(time_series_data):
-    # Be default, we would use 'db1' as our wavelet object
+# the parameter of this function has to be a matrix, cannot be an simple array
+def dwt_haar_transformation(time_series_data):
+    # Be default, we would use 'haar' as our wavelet object
     # the DWT function will return approximation and detail coefficients
-    return pywt.dwt(time_series_data, 'db1')
-
-
-print(dwt(data))
+    return pywt.dwt(time_series_data, 'haar')
 
