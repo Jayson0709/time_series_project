bee6c109664809808597561e0370c1821e791f66
Jayson Zhou <jaysonz6879@gmail.com>
2022-02-05 01:09:38 +0000

    [Update] Implement the Piecewise Linear Aggregation Approximation algorithm.

--- static/Python/pla.py
+++ static/Python/pla.py
@@ -1,2 +1,51 @@
-# Piecewise Linear Aggregation
+# Piecewise Linear Aggregate Approximation
+import pandas as pd
+import numpy as np
+import statistics
 
+# Get sample data
+n = r"../../datasets/Beef_TRAIN"
+data = pd.read_csv(n).to_numpy()[3][1:]
+
+
+def calculate_covariance(array_x, array_y, bias=False):
+    result = 0.0
+    length = len(array_x)
+    if length != len(array_y):
+        raise ValueError('length of array x is not equal to array y --- dimensions mismatch.')
+    elif length < 2:
+        raise ValueError('array x is too small --- insufficient observed points in sample.')
+    else:
+        x_mean = np.mean(array_x)
+        y_mean = np.mean(array_y)
+        for i in range(length):
+            x_deviation = array_x[i] - x_mean
+            y_deviation = array_y[i] - y_mean
+            result += (x_deviation * y_deviation - result) / (i + 1)
+    return result * (length / (length - 1)) if bias else result
+
+
+# TODO In the webpage, use a slide for user input to define the value of "segments"
+def pla_transformation(time_series_data, segments):
+    length = len(time_series_data)
+    if length < segments:
+        raise ValueError('input array length is smaller than segments value.')
+
+    if length % segments != 0:
+        raise ValueError('input array length is not divisible to segments.')
+
+    reduced_data = [[0.0, 0.0]] * segments
+    segment_size = length // segments
+    x = [0.0] * segment_size
+    for i in range(segment_size):
+        x[i] = i + 1
+
+    variance = statistics.variance(x)
+    for i in range(segments):
+        y = time_series_data[i * segment_size: (i + 1) * segment_size]
+        covariance = calculate_covariance(x, y, True)
+        mean = np.mean(y)
+        reduced_data[i] = [mean, covariance / variance]
+
+    # each of element of the array will be like a list of [mean, slope]
+    return reduced_data
