75b5f5f9eb986d1e3b1d527ac2e4847de0dee344
Jayson Zhou <jaysonz6879@gmail.com>
2022-02-07 00:17:14 +0000

    [Update] iterate the dft algorithm and made it become a class file.

--- static/Python/dft.py
+++ static/Python/dft.py
@@ -7,7 +7,12 @@ n = r"../../datasets/Beef_TRAIN"
 data = pd.read_csv(n).to_numpy()
 
 
-# the input parameter can be one array or be one matrix
-def dft_transformation(time_series_data):
-    return np.fft.fftn(time_series_data)
+class DiscreteFourierTransformation:
 
+    def __init__(self):
+        pass
+
+    # the input parameter can be one array or be one matrix
+    @staticmethod
+    def transform(time_series_data):
+        return np.fft.fftn(time_series_data)
