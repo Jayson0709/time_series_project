75b8db801d68edf7931fa1f923e97301a3e50c7c
Jayson Zhou <jaysonz6879@gmail.com>
2022-02-07 00:24:27 +0000

    [Update] iterate the SVD algorithm and made it become a class file.

--- static/Python/svd.py
+++ static/Python/svd.py
@@ -8,15 +8,20 @@ n = r"../../datasets/Beef_TRAIN"
 data = pd.read_csv(n).to_numpy()
 
 
-# TODO In the webpage, use a slide for user input to define the value of "n_elements"
-def svd_transformation(time_series_data, n_elements):
-    # Singular Value Decomposition
-    U, s, VT = svd(time_series_data)
-    # create m x n Sigma matrix
-    Sigma = np.zeros((time_series_data.shape[0], time_series_data.shape[1]))
-    # populate Sigma with n x n diagonal matrix
-    Sigma[: time_series_data.shape[0], : time_series_data.shape[0]] = np.diag(s)
-    # select based on the input n_elements to do dimensionality reduction
-    Sigma = Sigma[:, : n_elements]
-    # Transform and return
-    return U @ Sigma
+class SingularValueDecomposition:
+    def __init__(self):
+        pass
+
+    # TODO In the webpage, use a slide for user input to define the value of "n_elements"
+    @staticmethod
+    def svd_transformation(time_series_data, n_elements):
+        # Singular Value Decomposition
+        U, s, VT = svd(time_series_data)
+        # create m x n Sigma matrix
+        Sigma = np.zeros((time_series_data.shape[0], time_series_data.shape[1]))
+        # populate Sigma with n x n diagonal matrix
+        Sigma[: time_series_data.shape[0], : time_series_data.shape[0]] = np.diag(s)
+        # select based on the input n_elements to do dimensionality reduction
+        Sigma = Sigma[:, : n_elements]
+        # Transform and return
+        return U @ Sigma
