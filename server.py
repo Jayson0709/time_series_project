c25dd49b5295f2a074d04668822a8a4fe89800a1
Jayson Zhou <jaysonz6879@gmail.com>
2022-02-20 17:08:53 +0000

    [Add] add constant_values.py and adjust the strings correspondingly.

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ from static.Python import dwt
 from static.Python import paa
 from static.Python import pla
 from static.Python import svd
+from static.Python import constant_values
 
 # Get the demo data from the datasets
 file_path = r"./datasets/Beef_TRAIN"
@@ -40,52 +41,51 @@ def visualization():
 
 @app.route("/visualization/APCA", methods=['GET', 'POST'])
 def apca_visualization():
-    title_name = 'APCA'
     x_data = []
     y_data = []
     reduced_data = []
     show_demo_message = True
-    segments = 12
+    segments = None
     if request.method == 'GET':
+        segments = 12
         APCA = apca.AdaptivePiecewiseConstantApproximation(segments)
         x_data = []
-        y_data = [float(i) for i in request.form.get('data').replace(" ", "").split(',')]
+        y_data = data[3]
         reduced_data = APCA.transform(y_data)
     elif request.method == 'POST':
         try:
             x_data = []
-            y_data = [float(i) for i in request.form.get('data').replace(" ", "").split(',')]
-            segments = int(request.form.get('segments'))
+            y_data = [float(i) for i in request.form.get(constant_values.STRING_DATA).replace(constant_values.SPACE, constant_values.EMPTY_STRING).split(constant_values.COMMA)]
+            segments = int(request.form.get(constant_values.STRING_SEGMENTS))
             APCA = apca.AdaptivePiecewiseConstantApproximation(segments)
             reduced_data = APCA.transform(y_data)
             show_demo_message = False
         except Exception as e:
-            flash(str(e), category='error')
+            flash(str(e), category=constant_values.STRING_CATEGORY_ERROR)
     line = (
         Line()
             .add_xaxis(xaxis_data=x_data)
-            .add_yaxis(series_name="Original Data",
+            .add_yaxis(series_name=constant_values.STRING_ORIGINAL_DATA,
                        y_axis=y_data,
                        symbol="emptyCircle",
                        is_symbol_show=True,
                        is_smooth=True,
                        label_opts=opts.LabelOpts(is_show=False),
                        )
-            .add_yaxis(series_name="Reduced Data",
+            .add_yaxis(series_name=constant_values.STRING_REDUCED_DATA,
                        y_axis=reduced_data,
                        symbol="emptyCircle",
                        is_symbol_show=True,
                        is_smooth=True,
                        label_opts=opts.LabelOpts(is_show=False),
                        )
-            .set_global_opts(title_opts=opts.TitleOpts(title=title_name))
+            .set_global_opts(title_opts=opts.TitleOpts(title=constant_values.APCA_TITLE))
     )
     return render_template("apca.html", line_options=line.dump_options(), original_data=y_data, reduced_data=reduced_data, segments=segments, boolean_message=show_demo_message)
 
 
 @app.route("/visualization/DFT", methods=['GET', 'POST'])
 def dft_visualization():
-    title_name = 'DFT'
     DFT = dft.DiscreteFourierTransformation()
     x_data = []
     y_data = []
@@ -98,29 +98,29 @@ def dft_visualization():
     elif request.method == 'POST':
         try:
             x_data = []
-            y_data = [float(i) for i in request.form.get('data').replace(" ", "").split(',')]
+            y_data = [float(i) for i in request.form.get(constant_values.STRING_DATA).replace(constant_values.SPACE, constant_values.EMPTY_STRING).split(constant_values.COMMA)]
             reduced_data = DFT.transform(y_data)
             show_demo_message = False
         except Exception as e:
-            flash(str(e), category='error')
+            flash(str(e), category=constant_values.STRING_CATEGORY_ERROR)
     line = (
         Line()
             .add_xaxis(xaxis_data=x_data)
-            .add_yaxis(series_name="Original Data",
+            .add_yaxis(series_name=constant_values.STRING_ORIGINAL_DATA,
                        y_axis=y_data,
                        symbol="emptyCircle",
                        is_symbol_show=True,
                        is_smooth=True,
                        label_opts=opts.LabelOpts(is_show=False),
                        )
-            .add_yaxis(series_name="Reduced Data",
+            .add_yaxis(series_name=constant_values.STRING_REDUCED_DATA,
                        y_axis=reduced_data,
                        symbol="emptyCircle",
                        is_symbol_show=True,
                        is_smooth=True,
                        label_opts=opts.LabelOpts(is_show=False),
                        )
-            .set_global_opts(title_opts=opts.TitleOpts(title=title_name))
+            .set_global_opts(title_opts=opts.TitleOpts(title=constant_values.DFT_TITLE))
     )
     return render_template("dft.html", line_options=line.dump_options(), original_data=y_data, reduced_data=reduced_data, boolean_message=show_demo_message)
 
@@ -128,7 +128,6 @@ def dft_visualization():
 @app.route("/visualization/DWT", methods=['GET', 'POST'])
 def dwt_visualization():
     DWT = dwt.DiscreteHaarWaveletTransformation()
-    title_name = 'DWT'
     x_data = []
     y_data = []
     reduced_data = []
@@ -140,36 +139,35 @@ def dwt_visualization():
     elif request.method == 'POST':
         try:
             x_data = []
-            y_data = [float(i) for i in request.form.get('data').replace(" ", "").split(',')]
+            y_data = [float(i) for i in request.form.get(constant_values.STRING_DATA).replace(constant_values.SPACE, constant_values.EMPTY_STRING).split(constant_values.COMMA)]
             reduced_data = DWT.haar_transformation(y_data)
             show_demo_message = False
         except Exception as e:
-            flash(str(e), category='error')
+            flash(str(e), category=constant_values.STRING_CATEGORY_ERROR)
     line = (
         Line()
             .add_xaxis(xaxis_data=x_data)
-            .add_yaxis(series_name="Original Data",
+            .add_yaxis(series_name=constant_values.STRING_ORIGINAL_DATA,
                        y_axis=y_data,
                        symbol="emptyCircle",
                        is_symbol_show=True,
                        is_smooth=True,
                        label_opts=opts.LabelOpts(is_show=False),
                        )
-            .add_yaxis(series_name="Reduced Data",
+            .add_yaxis(series_name=constant_values.STRING_REDUCED_DATA,
                        y_axis=reduced_data,
                        symbol="emptyCircle",
                        is_symbol_show=True,
                        is_smooth=True,
                        label_opts=opts.LabelOpts(is_show=False),
                        )
-            .set_global_opts(title_opts=opts.TitleOpts(title=title_name))
+            .set_global_opts(title_opts=opts.TitleOpts(title=constant_values.DWT_TITLE))
     )
-    return render_template("dft.html", line_options=line.dump_options(), original_data=y_data, reduced_data=reduced_data, boolean_message=show_demo_message)
+    return render_template("dwt.html", line_options=line.dump_options(), original_data=y_data, reduced_data=reduced_data, boolean_message=show_demo_message)
 
 
 @app.route("/visualization/PAA", methods=['GET', 'POST'])
 def paa_visualization():
-    title_name = 'PAA'
     x_data = []
     y_data = []
     reduced_data = []
@@ -183,39 +181,38 @@ def paa_visualization():
         reduced_data = PAA.transform(y_data)
     elif request.method == 'POST':
         try:
-            y_data = [float(i) for i in request.form.get('data').replace(" ", "").split(',')]
+            y_data = [float(i) for i in request.form.get(constant_values.STRING_DATA).replace(constant_values.SPACE, constant_values.EMPTY_STRING).split(constant_values.COMMA)]
             x_data = [i for i in range(len(y_data))]
-            segments = int(request.form.get('segments'))
+            segments = int(request.form.get(constant_values.STRING_SEGMENTS))
             PAA = paa.PiecewiseAggregateApproximation(segments)
             reduced_data = PAA.transform(y_data)
             show_demo_message = False
         except Exception as e:
-            flash(str(e), category='error')
+            flash(str(e), category=constant_values.STRING_CATEGORY_ERROR)
     line = (
         Line()
             .add_xaxis(xaxis_data=x_data)
-            .add_yaxis(series_name="Original Data",
+            .add_yaxis(series_name=constant_values.STRING_ORIGINAL_DATA,
                        y_axis=y_data,
                        symbol="emptyCircle",
                        is_symbol_show=True,
                        is_smooth=True,
                        label_opts=opts.LabelOpts(is_show=False),
                        )
-            .add_yaxis(series_name="Reduced Data",
+            .add_yaxis(series_name=constant_values.STRING_REDUCED_DATA,
                        y_axis=reduced_data,
                        symbol="emptyCircle",
                        is_symbol_show=True,
                        is_smooth=True,
                        label_opts=opts.LabelOpts(is_show=False),
                        )
-            .set_global_opts(title_opts=opts.TitleOpts(title=title_name))
+            .set_global_opts(title_opts=opts.TitleOpts(title=constant_values.PAA_TITLE))
     )
     return render_template("paa.html", line_options=line.dump_options(), original_data=y_data, reduced_data=reduced_data, segments=segments, boolean_message=show_demo_message)
 
 
 @app.route("/visualization/PLA", methods=['GET', 'POST'])
 def pla_visualization():
-    title_name = 'PLA'
     x_data = []
     y_data = []
     reduced_data = []
@@ -229,38 +226,37 @@ def pla_visualization():
     elif request.method == 'POST':
         try:
             x_data = []
-            y_data = [float(i) for i in request.form.get('data').replace(" ", "").split(',')]
-            segments = int(request.form.get('segments'))
+            y_data = [float(i) for i in request.form.get(constant_values.STRING_DATA).replace(constant_values.SPACE, constant_values.EMPTY_STRING).split(constant_values.COMMA)]
+            segments = int(request.form.get(constant_values.STRING_SEGMENTS))
             PLA = pla.PiecewiseLinearAggregateApproximation(segments)
             reduced_data = PLA.transform(y_data)
             show_demo_message = False
         except Exception as e:
-            flash(str(e), category='error')
+            flash(str(e), category=constant_values.STRING_CATEGORY_ERROR)
     line = (
         Line()
             .add_xaxis(xaxis_data=x_data)
-            .add_yaxis(series_name="Original Data",
+            .add_yaxis(series_name=constant_values.STRING_ORIGINAL_DATA,
                        y_axis=y_data,
                        symbol="emptyCircle",
                        is_symbol_show=True,
                        is_smooth=True,
                        label_opts=opts.LabelOpts(is_show=False),
                        )
-            .add_yaxis(series_name="Reduced Data",
+            .add_yaxis(series_name=constant_values.STRING_REDUCED_DATA,
                        y_axis=reduced_data,
                        symbol="emptyCircle",
                        is_symbol_show=True,
                        is_smooth=True,
                        label_opts=opts.LabelOpts(is_show=False),
                        )
-            .set_global_opts(title_opts=opts.TitleOpts(title=title_name))
+            .set_global_opts(title_opts=opts.TitleOpts(title=constant_values.PLA_TITLE))
     )
     return render_template("pla.html", line_options=line.dump_options(), original_data=y_data, reduced_data=reduced_data, segments=segments, boolean_message=show_demo_message)
 
 
 @app.route("/visualization/SVD", methods=['GET', 'POST'])
 def svd_visualization():
-    title_name = 'SVD'
     SVD = svd.SingularValueDecomposition()
     x_data = []
     y_data = []
@@ -274,30 +270,30 @@ def svd_visualization():
     elif request.method == 'POST':
         try:
             x_data = []
-            y_data = [float(i) for i in request.form.get('data').replace(" ", "").split(',')]
-            n_elements = int(request.form.get('n_elements'))
+            y_data = [float(i) for i in request.form.get(constant_values.STRING_DATA).replace(constant_values.SPACE, constant_values.EMPTY_STRING).split(constant_values.COMMA)]
+            n_elements = int(request.form.get(constant_values.STRING_N_ELEMENTS))
             reduced_data = SVD.transform(y_data, n_elements)
             show_demo_message = False
         except Exception as e:
-            flash(str(e), category='error')
+            flash(str(e), category=constant_values.STRING_CATEGORY_ERROR)
     line = (
         Line()
             .add_xaxis(xaxis_data=x_data)
-            .add_yaxis(series_name="Original Data",
+            .add_yaxis(series_name=constant_values.STRING_ORIGINAL_DATA,
                        y_axis=y_data,
                        symbol="emptyCircle",
                        is_symbol_show=True,
                        is_smooth=True,
                        label_opts=opts.LabelOpts(is_show=False),
                        )
-            .add_yaxis(series_name="Reduced Data",
+            .add_yaxis(series_name=constant_values.STRING_REDUCED_DATA,
                        y_axis=reduced_data,
                        symbol="emptyCircle",
                        is_symbol_show=True,
                        is_smooth=True,
                        label_opts=opts.LabelOpts(is_show=False),
                        )
-            .set_global_opts(title_opts=opts.TitleOpts(title=title_name))
+            .set_global_opts(title_opts=opts.TitleOpts(title=constant_values.SVD_TITLE))
     )
     return render_template("svd.html", line_options=line.dump_options(), original_data=y_data, reduced_data=reduced_data, n_elements=n_elements, boolean_message=show_demo_message)
 
