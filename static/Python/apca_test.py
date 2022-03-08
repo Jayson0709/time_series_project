28adbf510dc2fdd64bc598acfd41e630dc280635
Jayson Zhou <jaysonz6879@gmail.com>
2022-03-08 10:06:48 +0000

    [Update] modification on the test code and add euclidean distance test and dynamic time warping distance test.

--- static/Python/apca_test.py
+++ static/Python/apca_test.py
@@ -2,6 +2,8 @@ import pandas as pd
 from memory_profiler import profile
 import time
 import apca
+from fastdtw import fastdtw
+from scipy.spatial.distance import euclidean
 
 # Get the test data
 fifty_words_file_path = r"./test_datasets/50words_TEST"
@@ -17,6 +19,19 @@ ECG_data = pd.read_csv(ECG_file_path, header=None).to_numpy()
 symbols_data = pd.read_csv(symbols_file_path, header=None).to_numpy()
 
 
+# Implement similarity search on two sets of time series data, calculate the accuracy based on Euclidean distance
+def graph_accuracy_based_on_ed(raw_dataset, transformed_dataset):
+    euclidean_distance = 0.0
+    for i in range(len(raw_dataset)):
+        euclidean_distance += abs(raw_dataset[i] - transformed_dataset[i])
+    return euclidean_distance
+
+
+# Implement similarity search on two sets of time series data, calculate the accuracy based on Dynamic Time Warping
+def graph_accuracy_based_on_dtw(raw_dataset, transformed_dataset):
+    return fastdtw(raw_dataset, transformed_dataset, dist=euclidean)
+
+
 @profile
 def test(data):
     start = time.time()
@@ -24,7 +39,10 @@ def test(data):
     apca_dataset, reduced_data = APCA.transform(data)
     end = time.time()
     print('execution time is {}'.format(end-start))
-    return apca_dataset, reduced_data
+    euclidean_distance = graph_accuracy_based_on_ed(data, apca_dataset)
+    dtw_distance, warping_path = graph_accuracy_based_on_dtw(data, apca_dataset)
+    print('Euclidean distance is {}'.format(euclidean_distance))
+    print('Dynamic Time Warping distance is {}'.format(dtw_distance))
 
 
 test(fifty_words_data[1])
--- static/Python/svd_test.py
+++ static/Python/svd_test.py
@@ -2,6 +2,8 @@ import pandas as pd
 from memory_profiler import profile
 import time
 import svd
+from fastdtw import fastdtw
+from scipy.spatial.distance import euclidean
 
 # Get the test data
 fifty_words_file_path = r"./test_datasets/50words_TEST"
@@ -17,18 +19,34 @@ ECG_data = pd.read_csv(ECG_file_path, header=None).to_numpy()
 symbols_data = pd.read_csv(symbols_file_path, header=None).to_numpy()
 
 
+# Implement similarity search on two sets of time series data, calculate the accuracy based on Euclidean distance
+def graph_accuracy_based_on_ed(raw_dataset, transformed_dataset):
+    euclidean_distance = 0.0
+    for i in range(len(raw_dataset)):
+        euclidean_distance += abs(raw_dataset[i] - transformed_dataset[i])
+    return euclidean_distance
+
+
+# Implement similarity search on two sets of time series data, calculate the accuracy based on Dynamic Time Warping
+def graph_accuracy_based_on_dtw(raw_dataset, transformed_dataset):
+    return fastdtw(raw_dataset, transformed_dataset, dist=euclidean)
+
+
 @profile
 def test(data, n_component):
     start = time.time()
     SVD = svd.SingularValueDecomposition()
     svd_dataset, reduced_data = SVD.transform(data, n_component)
     end = time.time()
-    print('execution time is {}'.format(end-start))
-    return svd_dataset, reduced_data
+    print('Execution time is {}'.format(end-start))
+    euclidean_distance = graph_accuracy_based_on_ed(data, svd_dataset)
+    dtw_distance, warping_path = graph_accuracy_based_on_dtw(data, svd_dataset)
+    print('Euclidean distance is {}'.format(euclidean_distance))
+    print('Dynamic Time Warping distance is {}'.format(dtw_distance))
 
 
-test(fifty_words_data, 30)
-test(computers_data, 10)
-test(earthquake_data, 20)
-test(ECG_data, 100)
-test(symbols_data, 50)
+test(fifty_words_data[1], 30)
+test(computers_data[1], 10)
+test(earthquake_data[1], 20)
+test(ECG_data[1], 100)
+test(symbols_data[1], 50)
