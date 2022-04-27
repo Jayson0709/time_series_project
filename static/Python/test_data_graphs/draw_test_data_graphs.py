5b0be8664d1a63151a7a5556808a42678b063b03
Jayson Zhou <jaysonz6879@gmail.com>
2022-04-27 21:05:09 +0100

    [Update] add code for drawing different n_coefficients.

--- static/Python/test_data_graphs/draw_test_data_graphs.py
+++ static/Python/test_data_graphs/draw_test_data_graphs.py
@@ -5,6 +5,8 @@ import numpy as np
 dataset_name_list = ["50Words", "Computers", "Earthquakes", "ECG5000", "Symbols"]
 patterns = ['/', '\\', '*', '-', 'O', '|', '.']
 
+########################################################################################
+# Time and space complexity graphs
 width = 0.12
 x_axis = np.arange(len(dataset_name_list))
 
@@ -58,7 +60,8 @@ plt.ylabel('Used Memory (MiB)')
 plt.legend()
 plt.show()
 
-
+########################################################################################
+# Euclidean and dynamic time warping distances for five datasets
 # Euclidean distance data
 apca_ed = [26.696, 107.612, 275.576, 17.236, 25.579]
 dft_ed = [41.395, 144.807, 287.761, 21.596, 14.135]
@@ -111,3 +114,57 @@ plt.xlabel('Datasets')
 plt.ylabel('Dynamic Time Warping Distances')
 plt.legend()
 plt.show()
+
+########################################################################################
+# Euclidean and dynamic time warping distances for DFT with different coefficients.
+# Euclidean distance data
+list1_ed = [44.632, 144.364, 290.920, 30.251, 16.263]
+list2_ed = [40.842, 118.312, 272.183, 14.969, 14.887]
+list3_ed = [41.550, 115.887, 263.102, 10.652, 14.520]
+list4_ed = [40.893, 109.477, 271.379, 10.065, 14.269]
+list5_ed = [40.361, 109.611, 259.981, 8.875, 13.947]
+list6_ed = [39.389, 108.632, 253.425, 6.605, 14.115]
+list7_ed = [37.806, 106.413, 244.446, 0.135, 13.855]
+
+plt.subplot(1, 2, 1)
+plt.bar(x_axis, list1_ed, width, label='n_coefficients=20', hatch=patterns[0])
+plt.bar(x_axis + width, list2_ed, width, label='n_coefficients=40', hatch=patterns[1])
+plt.bar(x_axis + width * 2, list3_ed, width, label='n_coefficients=60', hatch=patterns[2])
+plt.bar(x_axis + width * 3, list4_ed, width, label='n_coefficients=80', hatch=patterns[3])
+plt.bar(x_axis + width * 4, list5_ed, width, label='n_coefficients=100', hatch=patterns[4])
+plt.bar(x_axis + width * 5, list6_ed, width, label='n_coefficients=120', hatch=patterns[5])
+plt.bar(x_axis + width * 6, list7_ed, width, label='n_coefficients=140', hatch=patterns[6])
+
+plt.xticks(x_axis + width * 3, dataset_name_list)
+plt.title('Euclidean Distances of DFT with Different n_coefficients')
+plt.xlabel('Datasets')
+plt.ylabel('Euclidean Distance')
+plt.legend()
+
+
+# Dynamic time warping distance data
+list1_dtw = [34.568, 129.263, 296.959, 20.700, 8.699]
+list2_dtw = [35.031, 112.255, 271.128, 12.509, 10.144]
+list3_dtw = [37.096, 113.295, 261.791, 9.637, 11.205]
+list4_dtw = [38.270, 108.539, 271.188, 9.646, 11.678]
+list5_dtw = [38.630, 109.489, 259.177,8.119, 12.157]
+list6_dtw = [38.093, 109.061, 251.924, 6.373, 12.520]
+list7_dtw = [36.959, 106.374, 243.237, 0.135, 12.715]
+
+plt.subplot(1, 2, 2)
+plt.bar(x_axis, list1_dtw, width, label='n_coefficients=20', hatch=patterns[0])
+plt.bar(x_axis + width, list2_dtw, width, label='n_coefficients=40', hatch=patterns[1])
+plt.bar(x_axis + width * 2, list3_dtw, width, label='n_coefficients=60', hatch=patterns[2])
+plt.bar(x_axis + width * 3, list4_dtw, width, label='n_coefficients=80', hatch=patterns[3])
+plt.bar(x_axis + width * 4, list5_dtw, width, label='n_coefficients=100', hatch=patterns[4])
+plt.bar(x_axis + width * 5, list6_dtw, width, label='n_coefficients=120', hatch=patterns[5])
+plt.bar(x_axis + width * 6, list7_dtw, width, label='n_coefficients=140', hatch=patterns[6])
+
+plt.xticks(x_axis + width * 3, dataset_name_list)
+plt.title('Dynamic Time Warping Distances of DFT with Different n_coefficients')
+plt.xlabel('Datasets')
+plt.ylabel('Dynamic Time Warping Distances')
+plt.legend()
+plt.show()
+
+########################################################################################
